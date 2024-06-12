840337119361e57baac1915a615959c1ea55024e
Sergio Martinez <elsamplio@msn.com>
2024-06-11 20:09:58 -0500

    Update
    TEsting the unit testing

--- src/components/Header/HeaderIntroduction.tsx
+++ src/components/Header/HeaderIntroduction.tsx
@@ -9,7 +9,7 @@ const HeaderIntroduction: React.FC<HeaderIntroductionProps> = ({ introduction })
             <div className='col-12 md:col-6 p-6 text-center md:text-left flex align-items-center '>
                 <section id='titleBlock'>
                     <span className='block text-6xl font-bold mb-1'>{introduction.name}</span>
-                    <div className='text-6xl text-primary font-bold mb-3'>{introduction.title}</div>
+                    <div className='text-6xl text-primary font-bold mb-3'>My Data</div>
                     <p className='mt-0 mb-4 text-700 line-height-3'>{introduction.headLine}</p>
                     <div className='card'>
                         <div className="flex flex-wrap-reverse md:flex-wrap card-container purple-container" style={{ maxWidth: 500 }}>
