e3ab712bcd5ef296ef5448d9562c500eedd3efcb
Sergio Martinez <elsamplio@msn.com>
2024-08-05 18:36:07 -0500

    Updates
    Changed html image for NextJS Image, library versions updates

--- src/components/Header/HeaderImage.tsx
+++ src/components/Header/HeaderImage.tsx
@@ -1,14 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const HeaderImage: React.FC = () => {
-    return <>
-        <div className='col-12 md:col-6 rightMainImage'>
-            <img src='https://i.ibb.co/2gRjZdS/SAMP-Profile.jpg'
-                alt='Sergio Andrés Martínez' className='md:ml-auto block md:h-full'
-                style={{ clipPath: 'polygon(15% 0, 100% 0%, 100% 100%, 0 100%)' }} />
-        </div>
+  return (
+    <>
+      <div className="col-12 md:col-6 rightMainImage">
+        <Image
+          src="/SAMP_Profile.jpg"
+          style={{ clipPath: "polygon(15% 0, 100% 0%, 100% 100%, 0 100%)" }}
+          alt="Sergio Andrés Martínez"
+          height={640} width={640}
+          className='md:ml-auto block md:h-full'
+        />
+      </div>
     </>
-}
+  );
+};
 
-export default HeaderImage
+export default HeaderImage;
